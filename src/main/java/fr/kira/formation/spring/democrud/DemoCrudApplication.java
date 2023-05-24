82d71d8111a112136d787f3ce757c7e8cd638b90
Killian Raoux <killian@semifir.com>
2023-05-24 15:00:15 +0200

    feat: mise en place des profiles

--- src/main/java/fr/kira/formation/spring/democrud/DemoCrudApplication.java
+++ src/main/java/fr/kira/formation/spring/democrud/DemoCrudApplication.java
@@ -2,12 +2,23 @@ package fr.kira.formation.spring.democrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
 
 @SpringBootApplication
 public class DemoCrudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoCrudApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DemoCrudApplication.class, args);
+		System.out.println(context.getBean("enDev"));
+	}
+
+	@Bean
+	@Profile("dev")
+	public String enDev(){
+		return "Profile Dev actif";
 	}
 
 }
--- src/main/java/fr/kira/formation/spring/democrud/SwaggerConfiguration.java
+++ src/main/java/fr/kira/formation/spring/democrud/SwaggerConfiguration.java
@@ -2,6 +2,7 @@ package fr.kira.formation.spring.democrud;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.builders.PathSelectors;
@@ -15,13 +16,24 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableWebMvc
 public class SwaggerConfiguration {
     @Bean
-    public Docket api(){
+    @Profile("dev")
+    public Docket apiDev(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build();
     }
+    @Bean
+    @Profile("!dev")
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.none())
+                .paths(PathSelectors.none())
+                .build();
+    }
+
 
     @Bean
     public InternalResourceViewResolver defaultViewResolver() {
